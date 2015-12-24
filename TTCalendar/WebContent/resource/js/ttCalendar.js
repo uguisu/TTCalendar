d48cb470db856f87410cc47570bf80ac22abeab7
xin.he <hexin0614@gmail.com>
2015-12-24 17:02:35 +0800

    Calculate the weekday of the first day

--- TTCalendar/WebContent/resource/js/ttCalendar.js
+++ TTCalendar/WebContent/resource/js/ttCalendar.js
@@ -10,7 +10,7 @@ function init() {
 	// TODO: For demo
 	$('#mainNavigationPrevButton').bind('click', function(){
 		commonPost("json/TAJ1000Action", "", function(jData) {
-			alert(jData.currentDate);
+			drawCalendarTable(jData.currentDate);
 		})
 	});
 	
@@ -61,4 +61,53 @@ function playAnimationLoading() {
  */
 function stopAnimationLoading() {
 	
+}
+
+/**
+ * Draw calendar table by according to a specified date(YYYYMMDD)
+ * @param dateYYYYMMDD Specified date(YYYYMMDD)
+ */
+function drawCalendarTable(dateYYYYMMDD) {
+	
+	// The first date of month
+	var firstDate = dateYYYYMMDD.substring(0, 6) + "01";
+	
+	alert(getZellerWeekDay(firstDate.substring(0, 4), firstDate.substring(4, 6), "01"));
+	
+	
+}
+
+/**
+ * Zeller's congruence
+ * 
+ * 
+ * h is the day of the week (0 = Saturday, 1 = Sunday, 2 = Monday, ..., 6 = Friday)
+ * q is the day of the month
+ * m is the month (3 = March, 4 = April, 5 = May, ..., 14 = February)
+ * K the year of the century (year mod 100).
+ * J is the zero-based century.
+ * 
+ * 
+ * @param intY
+ * @param intM
+ * @param intD
+ * @return
+ */
+function getZellerWeekDay(intY, intM, intD) {
+	
+	// Return value
+	var RTN = 0;
+	
+	/** q is the day of the month */
+	var q = parseInt(intD);
+	var m = parseInt(intM);
+	m = (m < 3) ? m + 12 : m;
+	var y = parseInt(intY);
+	y = (m < 3) ? y - 1 : y;
+	var K = y % 100;
+	var J = parseInt(y / 100);
+	
+	RTN = (q + 13 * parseInt((m + 1) / 5) + K + parseInt(K / 4) + parseInt(J / 4) - 2 * J) % 7;
+	
+	return RTN;
 }
