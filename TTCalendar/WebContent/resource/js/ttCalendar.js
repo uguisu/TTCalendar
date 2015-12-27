02c67cd084f494696c2405eb91a08e9ad4f61e6f
hexin0614@gmail.com <hexin0614@gmail.com>
2015-12-27 21:44:00 +0800

    Draw default calendar table works.

--- TTCalendar/WebContent/resource/js/ttCalendar.js
+++ TTCalendar/WebContent/resource/js/ttCalendar.js
@@ -3,8 +3,19 @@
  * Copyright 2015 hexin0614@gmail.com
  */
 
+/** Weekday name array */
+var WEEK_DAY = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+var WEEK_DAY_REORDER = new Array(6, 0, 1, 2, 3, 4, 5);
+/** Total amount of days in each month(The first element is an useless element) */
+var DAY_AMOUNT_ARRAY = new Array(Number.MIN_VALUE, 31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
 
-var WEEK_DAY = new Array("Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+/** Split string for formatDate() */
+var DATE_SPLIT_DEFAULT = new Array("", "", "");
+var DATE_SPLIT_CN = new Array("年", "月", "日");
+var DATE_SPLIT_EN = new Array("/", "/", "");
+
+/** Array for calendar table */
+var calendarTable = new Array();
 
 function init() {
 	
@@ -75,30 +86,74 @@ function drawCalendarTable(jData) {
 	// The first date of month
 	var firstDate = jData.currentDate.substring(0, 6) + "01";
 	
-	var _yyyyTMP = firstDate.substring(0, 4);
+	var _yyyyTmp = firstDate.substring(0, 4);
+	var _yyyyTmpVal = parseInt(_yyyyTmp);
 	var _mmTmp = firstDate.substring(4, 6);
+	var _mmTmpVal = parseInt(_mmTmp);
+	
+	var lengthOfMonth = getLengthOfMonth(_yyyyTmpVal, _mmTmpVal);
 	
 	// Weekday of the first date
-	var weekdayOfFirstDate = getZellerWeekDay(_yyyyTMP, _mmTmp, "01");
-	// Weekday of the last date
-	var weekdayOfLastDate = getZellerWeekDay(_yyyyTMP, _mmTmp, jData.lengthOfMonth);
+	var weekdayOfFirstDate = getZellerWeekDay(_yyyyTmpVal, _mmTmpVal, 1);
+  	// Weekday of the last date
+	var weekdayOfLastDate = getZellerWeekDay(_yyyyTmpVal, _mmTmpVal, lengthOfMonth);
+	// Split String Array
+	var splitStringArray = DATE_SPLIT_CN;
 	
-	// For debug
-	alert(WEEK_DAY[weekdayOfFirstDate]);
-	alert(WEEK_DAY[weekdayOfLastDate]);
-	alert(jData.lengthOfMonth);
+	calendarTable = new Array(5);
+	var i, j, dayCount = 1;
+
+	// Current month
+	// [1]The first week
+	calendarTable[0] = new Array(7);
+	for(j = weekdayOfFirstDate; j < 7; j++) {
+		calendarTable[0][j] = formatDate("", _mmTmpVal, dayCount++, splitStringArray);
+	}
+	// [2]The following weeks
+	for(i = 1; i < 5; i++) {
+		calendarTable[i] = new Array(7);
+		
+		for(j = 0; j < 7; j++) {
+			calendarTable[i][j] = formatDate("", _mmTmpVal, dayCount++, splitStringArray);
+			if(dayCount > lengthOfMonth) break;
+		}
+	}
+	// [3]Previous month
+	var _preYearMonth = getNextYearMonth(_yyyyTmpVal, _mmTmpVal, -1);
+	var _preYyyyTmpVal = parseInt(_preYearMonth.substring(0, 4));
+	var _preMmTmpVal = parseInt(_preYearMonth.substring(4, 6));
+	var _prelengthOfMonth = getLengthOfMonth(_preYyyyTmpVal, _preMmTmpVal);
+	for(j = weekdayOfFirstDate - 1; j >= 0; j--) {
+		calendarTable[0][j] = formatDate("", _preMmTmpVal, _prelengthOfMonth--, splitStringArray);
+	}
+	// [4]Next month
+	var _nextYearMonth = getNextYearMonth(_yyyyTmpVal, _mmTmpVal, 1);
+	var _nextYyyyTmpVal = parseInt(_nextYearMonth.substring(0, 4));
+	var _nextMmTmpVal = parseInt(_nextYearMonth.substring(4, 6));
+	var _nextlengthOfMonth = getLengthOfMonth(_nextYyyyTmpVal, _nextMmTmpVal);
+	i = 1;
+	for(j = weekdayOfLastDate + 1; j < 7; j++) {
+		calendarTable[4][j] = formatDate("", _nextMmTmpVal, i++, splitStringArray);
+	}
+	
+	// Draw to the page
+	var normalLineName = "";
+	for(i = 0; i < 5; i++) {
+		for(j = 0; j < 7; j++) {
+			normalLineName = "#normalLine" + i + j;
+			$(normalLineName).html(calendarTable[i][j]);
+		}
+	}
 }
 
 /**
  * Zeller's congruence
  * 
- * 
- * h is the day of the week (0 = Saturday, 1 = Sunday, 2 = Monday, ..., 6 = Friday)
+ * h is the day of the week (0 = Sunday, 1 = Monday, ..., 5 = Friday, 6 = Saturday)
  * q is the day of the month
  * m is the month (3 = March, 4 = April, 5 = May, ..., 14 = February)
  * K the year of the century (year mod 100).
  * J is the zero-based century.
- * 
  * 
  * @param intY
  * @param intM
@@ -111,15 +166,86 @@ function getZellerWeekDay(intY, intM, intD) {
 	var RTN = 0;
 	
 	/** q is the day of the month */
-	var q = parseInt(intD);
-	var m = parseInt(intM);
+	var q = intD;
+	var m = intM;
 	m = (m < 3) ? m + 12 : m;
-	var y = parseInt(intY);
+	var y = intY;
 	y = (m < 3) ? y - 1 : y;
 	var K = y % 100;
 	var J = parseInt(y / 100);
 	
 	RTN = (q + 13 * parseInt((m + 1) / 5) + K + parseInt(K / 4) + parseInt(J / 4) - 2 * J) % 7;
 	
-	return RTN;
+	return WEEK_DAY_REORDER[RTN];
+}
+
+/**
+ * Get length Of month
+ * @param year
+ * @param monthIndex
+ * @returns how many days in the month
+ */
+function getLengthOfMonth(year, monthIndex) {
+	
+	if(2 != monthIndex) {
+		return DAY_AMOUNT_ARRAY[monthIndex]
+	} else {
+		// Leap year check
+		if(0 == (year % 400)) {
+			return 29;
+		} else if(0 == (year % 4) && 0 != (year % 100)) {
+			return 29;
+		} else {
+			return 28;
+		}
+	}
+}
+
+/**
+ * Format date to string
+ * @param strYear
+ * @param intMonth
+ * @param intDay
+ * @param splitStringArray
+ * @param attachHeadZeroFlag True, if attach "0" to the head; otherwise use space instead. Default is "False".
+ */
+function formatDate(strYear, intMonth, intDay, splitStringArray, attachHeadZeroFlag) {
+	
+	var _splitStringArray = (null == splitStringArray || 3 != splitStringArray.length) ? DATE_SPLIT_DEFAULT : splitStringArray;
+	var _attachHead = (null == attachHeadZeroFlag || false == attachHeadZeroFlag) ? " " : "0";
+	var _month = (10 > intMonth) ? _attachHead + intMonth : "" + intMonth;
+	var _day = (10 > intDay) ? _attachHead + intDay : "" + intDay;
+	
+	return ((null == strYear || "" == strYear) ? "" : strYear + _splitStringArray[0]) + _month + _splitStringArray[1] + _day + _splitStringArray[2];
+}
+
+/**
+ * Calculate next year and month by offset
+ * @param year
+ * @param month
+ * @param offSet month offset
+ * @returns "YYYYMM"
+ */
+function getNextYearMonth(year, month, offSet) {
+	
+	var _year = year;
+	var _month = month;
+	
+	_year = _year + parseInt(offSet / 12);
+	_month = _month + (offSet % 12);
+
+	if(0 > offSet) {
+		// Pre
+		if(0 >= _month) {
+			_month += 12;
+			_year -= 1;
+		}
+	} else {
+		// Next
+		if(12 < _month) {
+			_month -= 12;
+			_year += 1;
+		}
+	}
+	return formatDate(_year, _month, 1, null, true).substring(0, 6);
 }
--- TTCalendar/WebContent/resource/page/main/mainCalendarTable.jsp
+++ TTCalendar/WebContent/resource/page/main/mainCalendarTable.jsp
@@ -13,71 +13,69 @@
             </tr>
         </table>
     </div>
-    
     <div id="line1" class="normalLine">
         <table id='CalendarTableline1' class='CalendarTableBaseStyle'>
             <tr>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
+                <td class='columnBaseStyle' id='normalLine00'></td>
+                <td class='columnBaseStyle' id='normalLine01'></td>
+                <td class='columnBaseStyle' id='normalLine02'></td>
+                <td class='columnBaseStyle' id='normalLine03'></td>
+                <td class='columnBaseStyle' id='normalLine04'></td>
+                <td class='columnBaseStyle' id='normalLine05'></td>
+                <td class='columnBaseStyle' id='normalLine06'></td>
             </tr>
         </table>
     </div>
     <div id="line2" class="normalLine">
         <table id='CalendarTableline2' class='CalendarTableBaseStyle'>
             <tr>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
+                <td class='columnBaseStyle' id='normalLine10'></td>
+                <td class='columnBaseStyle' id='normalLine11'></td>
+                <td class='columnBaseStyle' id='normalLine12'></td>
+                <td class='columnBaseStyle' id='normalLine13'></td>
+                <td class='columnBaseStyle' id='normalLine14'></td>
+                <td class='columnBaseStyle' id='normalLine15'></td>
+                <td class='columnBaseStyle' id='normalLine16'></td>
             </tr>
         </table>
     </div>
     <div id="line3" class="normalLine">
         <table id='CalendarTableline3' class='CalendarTableBaseStyle'>
             <tr>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
+                <td class='columnBaseStyle' id='normalLine20'></td>
+                <td class='columnBaseStyle' id='normalLine21'></td>
+                <td class='columnBaseStyle' id='normalLine22'></td>
+                <td class='columnBaseStyle' id='normalLine23'></td>
+                <td class='columnBaseStyle' id='normalLine24'></td>
+                <td class='columnBaseStyle' id='normalLine25'></td>
+                <td class='columnBaseStyle' id='normalLine26'></td>
             </tr>
         </table>
     </div>
     <div id="line4" class="normalLine">
         <table id='CalendarTableline4' class='CalendarTableBaseStyle'>
             <tr>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
+                <td class='columnBaseStyle' id='normalLine30'></td>
+                <td class='columnBaseStyle' id='normalLine31'></td>
+                <td class='columnBaseStyle' id='normalLine32'></td>
+                <td class='columnBaseStyle' id='normalLine33'></td>
+                <td class='columnBaseStyle' id='normalLine34'></td>
+                <td class='columnBaseStyle' id='normalLine35'></td>
+                <td class='columnBaseStyle' id='normalLine36'></td>
             </tr>
         </table>
     </div>
     <div id="line5" class="normalLine">
         <table id='CalendarTableline5' class='CalendarTableBaseStyle'>
             <tr>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
-                <td class='columnBaseStyle'></td>
+                <td class='columnBaseStyle' id='normalLine40'></td>
+                <td class='columnBaseStyle' id='normalLine41'></td>
+                <td class='columnBaseStyle' id='normalLine42'></td>
+                <td class='columnBaseStyle' id='normalLine43'></td>
+                <td class='columnBaseStyle' id='normalLine44'></td>
+                <td class='columnBaseStyle' id='normalLine45'></td>
+                <td class='columnBaseStyle' id='normalLine46'></td>
             </tr>
         </table>
     </div>
-    
 </div>
