ea06d629ab39178032c1e625f1dab3e533a87b31
hexin0614@gmail.com <hexin0614@gmail.com>
2015-12-27 21:45:15 +0800

    "lengthOfMonth" moved to page javascript.

--- TTCalendar/src/com/ttCalendar/json/action/TAJ1000Action.java
+++ TTCalendar/src/com/ttCalendar/json/action/TAJ1000Action.java
@@ -4,7 +4,6 @@
 package com.ttCalendar.json.action;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
@@ -16,7 +15,6 @@ import com.ttCalendar.core.BaseAction;
  * [JSON SAMPLE]<br/>
  *     {<br/>
  *         "currentDate":"20151119",<br/>
- *         "lengthOfMonth":"30"<br/>
  *     }
  * @author hexin0614@gmail.com
  */
@@ -28,8 +26,6 @@ public class TAJ1000Action extends BaseAction {
 	/** Page variables */
 	/** Current date */
 	private String currentDate;
-	/** Length of current month */
-	private int lengthOfMonth;
 	
 	/*
 	 * (non-Javadoc)
@@ -42,12 +38,8 @@ public class TAJ1000Action extends BaseAction {
 				TTCalendarStaticValue.FORMAT_STYLE_YYYYMMDD, Locale.PRC);
 		currentDate = LocalDate.now().format(dtf);
 		
-		Month month = Month.of(Integer.valueOf((currentDate.substring(4, 6))));
-		lengthOfMonth = month.length(true);
-
 		// GC
 		dtf = null;
-		month = null;
 		
 		return SUCCESS;
 	}
@@ -57,11 +49,5 @@ public class TAJ1000Action extends BaseAction {
 	}
 	public void setCurrentDate(String currentDate) {
 		this.currentDate = currentDate;
-	}
-	public int getLengthOfMonth() {
-		return lengthOfMonth;
-	}
-	public void setLengthOfMonth(int lengthOfMonth) {
-		this.lengthOfMonth = lengthOfMonth;
 	}
 }
