a6fddb4d8439541278073dfcd5d720551e463ed4
hexin0614@gmail.com <hexin0614@gmail.com>
2016-08-27 21:55:20 +0800

    Format code.

--- TTCalendar/WebContent/resource/js/ttCalendar.js
+++ TTCalendar/WebContent/resource/js/ttCalendar.js
@@ -322,9 +322,8 @@ function mouseWheelEventForMonth(e) {
  */
 function mouseWheelEventForYear(e) {
 	
-    var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||  // chrome
-																								// & ie
-	(e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));            // firefox
+    var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||  // chrome & ie
+	(e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1)); // firefox
     
 	if(delta > 0) {
 		// wheel up
